0699ae4232ee0d2a6ad021dc741c4931f5536bfb
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-18 00:52:11 +0530

    Rest & Spread

--- FirstNodeApp/play2.js
+++ FirstNodeApp/play2.js
@@ -6,6 +6,31 @@ const person = {
     }
 };
 
+const copiedPerson = {...person };
+console.log(copiedPerson);
 
 person.greet();
 console.log(person.name);
+
+const hobbies = ['Sports', 'Cooking'];
+// for (let hobby of hobbies) {
+//     console.log(hobby);
+// }
+
+console.log(hobbies);
+console.log(hobbies.map(hobby => {
+    return 'Hobby: ' + hobby
+}));
+
+hobbies.push('programming');
+console.log(hobbies);
+
+// const copiedArray = hobbies.slice();
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => {
+    return args;
+};
+
+console.log(toArray(1, 2, 3, 4));

fea9c48b9d322aebbf81a2ae75e755a6f0929e66
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-18 00:59:22 +0530

    Destructing Complete

--- FirstNodeApp/play3.js
+++ FirstNodeApp/play3.js
@@ -6,8 +6,21 @@ const person = {
     }
 };
 
-const printName = (person) => {
+/*const printName = (person) => {
     console.log(person.name);
 }
 
+printName(person);*/
+
+const printName = ({ name, age }) => {
+    console.log(name, age);
+}
+
 printName(person);
+
+const { name, age } = person
+console.log(name, age);
+
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);

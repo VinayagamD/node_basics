8b43c658a8da41d9d77fe521bab7b52d6d0485b1
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-19 23:05:12 +0530

    added express routes

--- express-js-tut/app.js
+++ express-js-tut/app.js
@@ -1,22 +1,28 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 
-app.use('/', (req, res, next) => {
-    console.log("This always runs");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/add-product', (req, res, next) => {
+    res.send(`
+        <form action="/product" method="POST" >
+            <input type="text" name="title">
+            <button type="submit">Add Product</button>
+        </form>
+    `);
 });
 
-
-app.use('/add-product', (req, res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>The Add Product Page</h1>');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>Hello From the Express!</h1>');
+    res.send('<h1>Hello From the Express!</>');
 });
 
 app.listen(3000);

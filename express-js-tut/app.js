9e1024f621c72700370ea416252bd31d017c8a59
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-05-20 00:09:52 +0530

    Added 404 page

--- express-js-tut/app.js
+++ express-js-tut/app.js
@@ -2,17 +2,18 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 

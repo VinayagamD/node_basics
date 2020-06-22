7fd071dc827bc0a182c2b50401a7463544f741da
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-23 03:35:34 +0530

    Added Scratch React App

---
+++ react-basics/react-basics-app/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo"/>
+          <p>Learn React Basics From Scratch</p>
+        </header>
+
+      </div>
+  );
+}
+
+
+export default App;

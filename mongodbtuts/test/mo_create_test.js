12f30364daf3e37e45cbc89ed79680f78394291b
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-22 23:47:32 +0530

    mocha test for read and create mongo db complete

---
+++ mongodbtuts/test/mo_create_test.js
@@ -0,0 +1,18 @@
+const Student = require('../app/student');
+const assert  = require('assert');
+
+describe('Create records', () => {
+    it('create a user in DB', () =>{
+        // assert(false);
+        const sam = new Student({name: "Sam"});
+        sam.save()
+            .then(()=>{
+                assert(!sam.isNew);
+            }).catch(() => {
+                console.log("error");
+        });
+    });
+});
+
+
+
---
+++ mongodbtuts/test/mo_read_test.js
@@ -0,0 +1,22 @@
+const Student = require('../app/student');
+const assert  = require('assert');
+
+// All read tests
+describe('Read Tests', ()=>{
+    let reader;
+    beforeEach((done)=>{
+        reader = Student({name: "Reader"});
+        reader.save()
+            .then(()=>{
+                done();
+            });
+    });
+
+    it("Read a user: Reader", (done) => {
+        Student.find({name: "Reader"}).then((students) => {
+            // id is a BSON value
+            assert(reader._id.toString() === students[0]._id.toString());
+            done();
+        });
+    });
+});
